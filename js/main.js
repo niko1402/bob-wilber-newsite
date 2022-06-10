5f7ef4c350bdce096259547f1c502b6737f6af32
nik_e_p <nikpayton@gmail.com>
2022-06-10 11:26:40 -0300

    Added navbar id in html. Added navbar.sticky in css with dark background and gold text, and corresponding JS code.

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const navbar = document.getElementById("navbar");
+// Find out how far the navbar is from the top of the screen
+const navPos = navbar.getBoundingClientRect().top;
+// Get the section links from the navbar
+const navbarLinks = document.querySelectorAll("li a");
+console.log(navbarLinks);
+
+// Add an event listener to detect scrolling, and then store the scroll position in a variable
+window.addEventListener("scroll", e => {
+  const scrollPos = window.scrollY;
+  // If the scroll position is greater than the navbar position, add the sticky class.
+  if (scrollPos > navPos) {
+    navbar.classList.add('sticky');
+  } else {
+    navbar.classList.remove('sticky');
+  };
+  
+  // Loop through links in navbar
+  navbarLinks.forEach(link => {
+    const section = document.querySelector(link.hash);
+    // Check if the section is currently onscreen. Add 150 to switch sections when the screen is almost showing 100% of that section
+    // If so, add active class
+    if (scrollPos + 150 > section.offsetTop && scrollPos + 150 < section.offsetTop + section.offsetHeight ) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
+    }
+  });
+});
